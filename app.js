059b138613762be2ced355f7c31376a04df03758
Nicholas Winters <nicholas.d.winters@gmail.com>
2021-07-28 15:45:19 -0500

    updating cors

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
 
     if (req.method === "OPTIONS") {
-        return res.status(200).end();
+        res.status(200);
+        return next();
     }
 
     return next();
