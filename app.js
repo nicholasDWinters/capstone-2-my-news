ef3236bba228a7bce081d7a6c5ae9c14bdf6b4b0
Nicholas Winters <nicholas.d.winters@gmail.com>
2021-07-28 14:29:45 -0500

    updated cors

--- app.js
+++ app.js
@@ -10,17 +10,17 @@ const app = express();
 
 
 
-app.use(function (req, res, next) {
-    // CORS headers
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+// app.use(function (req, res, next) {
+//     // CORS headers
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
 
-    if (req.method === "OPTIONS") {
-        return res.status(200).end();
-    }
+//     if (req.method === "OPTIONS") {
+//         return res.status(200).end();
+//     }
 
-    return next();
-})
+//     return next();
+// })
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
